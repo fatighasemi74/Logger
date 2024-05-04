7a4ad571b5fc64433b2c8e4c67eff2013888a0b5
fati <faateme.ghasemii@gmail.com>
2024-05-04 18:12:59 -0400

    logger as middleware

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from logger import logger
+from middleware import log_middleware
+from starlette.middleware.base import BaseHTTPMiddleware
+import asyncio
 
 app = FastAPI()
+app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
 logger.info('starting app........')
+
 
 @app.get("/")
 async def index() -> dict:
-    logger.info('request to indext page')
     return {'message': 'hello'}
 
 @app.get("/upload-videos")
 async def index() -> dict:
-    logger.info('request to upload videos')
+    await asyncio.sleep(1.5)
     return {'message': 'upload videos'}
