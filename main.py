6f1cd1f9e28cd60091fa31d0702541d2e7fccaf5
fati <faateme.ghasemii@gmail.com>
2024-05-07 01:44:36 -0400

    debug.log backup

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import json
 import logging
 import logging.config
 from fastapi import FastAPI, HTTPException
-from logger import logger
-from starlette.middleware.base import BaseHTTPMiddleware
+from pydantic import BaseModel
 
 # load logging configuration
 with open('logging_config.json', 'r') as config_file:
@@ -15,6 +14,16 @@ logger = logging.getLogger("my_app")
 
 app = FastAPI()
 
+# This will act as our fake database for now
+fake_db = {}
+
+# Pydantic model to represent our data structure
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    on_offer: bool = False
+
 @app.get("/")
 async def read_root():
     try:
@@ -24,13 +33,35 @@ async def read_root():
         logger.exception("an error occurred in the root endpoint")
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/item/{item_id}")
-def read_item(item_id: int):
+@app.post("/item/{item_id}", response_model=Item)
+async def create_item(item_id: int, item:Item):
     try:
-        logger.debug(f"item endpoint was called with item_id: {item_id}")
-        return {"item_id": item_id}
-    except Exception as e:
-        logger.exception(f"an error occurred in the item endpoint for item_id: {item_id}")
-        raise HTTPException(status_code=500, detail=str(e))
+        if item_id in fake_db:
+            logger.info(f"item exists")
+            raise HTTPException(status_code=400, detail="Item exists.")
+        fake_db[item_id] = item
+        logger.info(f"created item with id: {item_id}")
+        return fake_db[item_id]
+    except HTTPException as http_exc:
+        logger.error(f"http error: {http_exc.detail}")
+        raise
+    except Exception as exc:
+        logger.exception(f"an error occurred: {exc}")
+        raise HTTPException(status_code=500, detail="an unexpected error")
+
+@app.get("/item/{item_id}", response_model=Item)
+async def read_item(item_id: int):
+    try:
+        if item_id not in fake_db:
+            raise HTTPException(status_code=404, detail="item not found.")
+        logger.info(f"Retrieved item with id: {item_id}", extra={"request_id": "some_unique_identifier"})
+        return fake_db[item_id]
+    except HTTPException as http_exc:
+        logger.error(f"an http error occurred: {http_exc.detail}")
+        raise
+    except Exception as exc:
+        logger.exception(f"an unexpected error: {exc}")
+        raise HTTPException(status_code=500, detail="an unexpected error")
+
 
 
